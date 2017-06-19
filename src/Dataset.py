7e670746eb802f89d77e1ec9b9b9952bd10bfc7d
Lyken17 <lykensyu@gmail.com>
2017-06-18 22:58:45 -0700

    first version done

--- src/Dataset.py
+++ src/Dataset.py
@@ -14,14 +14,30 @@ from PIL import Image
 import os.path as osp
 import matplotlib.pyplot as plt
 
-
 IMG_EXTENSIONS = [
     '.jpg', '.JPG', '.jpeg', '.JPEG',
     '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
 ]
 
+
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+
+class ImageFolder(mx.io.DataIter):
+    '''
+    To read  
+
+    Example:
+        root/dog/xx01.jpg
+        root/dog/xx02.jpg
+
+        root/cat/xx01.jpg
+        root/cat/xx02.jpg
+    '''
+
+    def __init__(self):
+        pass
 
 
 class SampleLoader(mx.io.DataIter):
@@ -76,19 +92,21 @@ class DataLoader(mx.io.DataIter):
         In mxnet, 5 functions below is necessary for implementing a DataLoader
     """
 
-    def __init__(self):
+    def __init__(self, read_threads=1):
         """
             set all required variables ready, see implementation below for more details 
         """
         super(DataLoader, self).__init__()
 
         # shape related variables
-        self.data_shape = (32, 3, 128, 12)
-        self.label_shape = (32, 1)
-        self.batch_size = self.data_shape[0]
-        self.image_size = self.data_shape[1:]
+        self.data_shapes = [mx.io.DataDesc(name='data', shape=(32, 3, 128, 128), layout='NCHW')]
+        self.label_shapes = [mx.io.DataDesc(name='softmax_label', shape=(32,), layout='N')]
+        self.batch_size = 32
+        # self.image_size = self.data_shape[1:]
 
         # list related variables
+        self.root = None
+
         self.loader_list = []  # TODO: add a function interface
         self.current = 0
         self.total = len(self.loader_list)
@@ -100,7 +118,7 @@ class DataLoader(mx.io.DataIter):
         # transformation
         self.transform = None
 
-        raise NotImplementedError('you must override __init__() you self')
+        # raise NotImplementedError('you must override __init__() you self')
 
     def next(self):
         """
@@ -114,33 +132,50 @@ class DataLoader(mx.io.DataIter):
         if self.current + self.batch_size > self.total:
             raise StopIteration  # reach epoch end
         else:
-            return self.load_batch()
+            return self.get_batch()
 
-        raise NotImplementedError('you must override next() you self')
+            # TODO: remove this comment when construction finishes
+            # raise NotImplementedError('you must override next() you self')
 
-    def load_batch(self):
+    def get_batch(self):
         # make it static, unreachable from outside
         index_list = range(self.current, self.current + self.batch_size)
 
         if self.read_threads > 1:
             p = Pool(self.read_threads)  # TODO: add pin memory to optimize speed
-            batch = p.map(self.get_single_pair, index_list)
+            batch = p.map(self.__getitem__, index_list)
         else:
             batch = []
             for ind in index_list:
-                batch.append(self.get_single_pair[ind])
+                batch.append(self.get_single_pair(ind))
+
+        # TODO: make batch here
+
         return batch
 
     def get_single_pair(self, index):
-        data = self.get_single_data(index)
-        label = self.get_single_label(index)
-        return [data, label]
+        # TODO: will be removed. Replace it by __getitem__
+        return self.__getitem__(index)
+
+    def __getitem__(self, index):
+        """
+        :param index(int): Index
+        :return: 
+            tuple: (data, label) where data and label are collections 
+        """
+        raise NotImplementedError('you must override __getitem__()')
 
     def get_single_data(self, index):
         raise NotImplemented
 
     def get_single_label(self, index):
         raise NotImplemented
+
+    def pil_loader(path):
+        # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+        with open(path, 'rb') as f:
+            with Image.open(f) as img:
+                return img.convert('RGB')
 
     @property
     def provide_data(self):
@@ -149,7 +184,8 @@ class DataLoader(mx.io.DataIter):
             [mx.io.DataDesc(), ... ]
                 A list of mx.io.DataDesc, which describes all data input blocks in network  
         """
-        raise NotImplementedError('you must override provide_data() you self')
+        return self.data_shapes
+        # raise NotImplementedError('you must override provide_data() ')
 
     @property
     def provide_label(self):
@@ -158,7 +194,8 @@ class DataLoader(mx.io.DataIter):
             [mx.io.DataDesc(), ... ]
                 A list of mx.io.DataDesc, which describes all label input blocks in network  
         """
-        raise NotImplementedError('you must override provide_label() you self')
+        return self.label_shapes
+        # raise NotImplementedError('you must override provide_label() ')
 
     def reset(self):
         """
@@ -168,19 +205,38 @@ class DataLoader(mx.io.DataIter):
         if self.random_shuffle:
             random.shuffle(self.loader_list)
 
+    def __len__(self):
+        return len(self.loader_list)
 
 
-class ImageFolder(mx.io.DataIter):
-    '''
-    To read  
-    
-    Example:
-        root/dog/xx01.jpg
-        root/dog/xx02.jpg
-        
-        root/cat/xx01.jpg
-        root/cat/xx02.jpg
-    '''
+class TestLoader(DataLoader):
+    def __init__(self, root="../data", transform=None, target_transform=None, batch_size=32):
+        super(TestLoader, self).__init__()
 
-    def __init__(self):
-        pass
+        self.batch_size = 32
+        self.data_shapes = [mx.io.DataDesc(name='data', shape=(32, 13, 128, 128), layout='NCHW')]
+        self.label_shapes = [mx.io.DataDesc(name='softmax_label', shape=(32,), layout='N')]
+
+        self.transform = None
+        self.label_transform = None
+        self.flist = []
+
+    def __getitem__(self, index):
+        path, label = self.flist[index]
+        data = self.pil_loader(path)
+
+        if self.transform is not None:
+            data = self.transform(data)
+
+        if self.label_transform is not None:
+            label = self.label_transform(label)
+
+        return data, label
+
+    def __len__(self):
+        return len(self.loader_list)
+
+
+if __name__ == "__main__":
+    loader = TestLoader()
+    print(loader.provide_data)
