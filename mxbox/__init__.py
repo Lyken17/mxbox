c1bbd657dabc72e5a7f5f9bfd6dd4e8a534bf968
lyken17 <lykensyu@gmail.com>
2017-09-17 20:01:11 -0700

    New function added : gpuManager

--- mxbox/__init__.py
+++ mxbox/__init__.py
@@ -2,6 +2,7 @@ from mxbox import models
 from mxbox import datasets
 from mxbox import transforms
 from mxbox import utils
+from mxbox import devices
 
 from mxbox.Dataset import Dataset
 from mxbox.DataLoader import DataLoader
---
+++ mxbox/devices/__init__.py
@@ -0,0 +1,1 @@
+from .gpuManager import gpuManager
